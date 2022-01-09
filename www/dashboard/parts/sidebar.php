7092d2c8465f0458c799130e5c580a8d36959b56
Aurelien Bonnal <aurelien@lajungle.fr>
2022-01-09 14:08:33 +0100

    Dashboard : URL Rewriting

--- www/dashboard/parts/sidebar.php
+++ www/dashboard/parts/sidebar.php
@@ -4,7 +4,7 @@
    </div>
    <nav class="text-white text-base  flex-grow font-semibold pt-1">
        <?php foreach($compteurs as $cpt): ?>
-           <a href="./detail.php?cpt=<?php echo $cpt->get('slug') ?>" class="flex items-center <?php echo ($cpt->get('slug') == $slug ? 'active-nav-link bg-blue-900' : '') ?> hover:bg-blue-600 text-white py-1 pl-6 nav-item">
+           <a href="./<?php echo $cpt->get('slug') ?>" class="flex items-center <?php echo ($cpt->get('slug') == $slug ? 'active-nav-link bg-blue-900' : '') ?> hover:bg-blue-600 text-white py-1 pl-6 nav-item">
                <?php echo $cpt->get('labelHTML') ?>
            </a>
        <?php endforeach ?>
@@ -12,7 +12,7 @@
    <div class="sticky bottom-0 bg-gray-800 p-2">
        <div class="text-center">
            <a href="./" class="pr-1"><i class="fas fa-home"></i></a>
-           <a href="./avg.php" class="pr-1"><i class="fas fa-chart-line"></i></a>
+           <a href="./moyennes-mobiles" class="pr-1"><i class="fas fa-chart-line"></i></a>
        </div>
    </div>
 </aside>
