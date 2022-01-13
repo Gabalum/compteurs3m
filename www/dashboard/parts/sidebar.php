cc9570b764f24a111e1a76e5e7399cf53851c53e
Aurelien Bonnal <aurelien@lajungle.fr>
2022-01-13 19:58:26 +0100

    Gestion responsive

--- www/dashboard/parts/sidebar.php
+++ www/dashboard/parts/sidebar.php
@@ -1,11 +1,15 @@
-<aside id="sidebar" class="absolute hidden sm:relative sm:block bg-slate-900 text-white h-screen flex flex-col sm:w-50  shadow-xl  overflow-y-scroll z-10">
+<aside id="sidebar" class="absolute hidden sm:relative sm:block bg-slate-900 text-white h-screen flex flex-col sm:w-60  shadow-xl  overflow-y-scroll z-10">
     <div class="p-2 pb-2 text-center ">
        <a href="./" class="text-white text-xl font-semibold uppercase hover:text-gray-300">Compteurs 3M</a>
    </div>
    <nav class="text-white text-sm flex-grow pt-1 pb-20">
        <?php foreach($compteurs as $cpt): ?>
            <a href="./<?php echo $cpt->get('slug') ?>" class="flex items-center <?php echo ($cpt->get('slug') == $slug ? 'active-nav-link bg-blue-900' : '') ?> hover:bg-blue-600 text-white py-1 pl-6 nav-item">
-               <?php echo $cpt->get('labelHTML') ?>
+               <?php if(stripos($cpt->get('labelHTML'), '(') > 0): ?>
+                   <?php echo str_replace('(', '<br class="hidden sm:block">(', $cpt->get('labelHTML')) ?>
+               <?php else: ?>
+                   <?php echo $cpt->get('labelHTML') ?>
+               <?php endif ?>
            </a>
        <?php endforeach ?>
    </nav>
